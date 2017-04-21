a21034c517271a9f4aa60021bbf6e3b9d2cc28b8
cipta <ciphwn@gmail.com>
2017-04-20 23:50:57 -0400

    tester

---
+++ models/do_test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+CMD="python autoencoder_main.py \
+	--vocabsize 10000 \
+	--max_sentences 35000  \
+        --supplement ../data/supplemental.csv \
+	--max_supplement 7000  \
+	--lr 0.001 \
+	--cuda \
+	--din 30 \
+	--dhid 50 \
+	--demb 50 \
+	--epochs 25 --batches 1000 --batchsize 80 \
+	--dropout 0.5 \
+	--nlayers 1 \
+	--squash_size 100 \
+	--noise_stdev 0.015 \
+	--sloss_factor 0.3 \
+	--sloss_slope 0.6 \
+	--sloss_shift 5 \
+	--dloss_factor 1.0 \
+	--dloss_slope 1 \
+	--dloss_shift 5 \
+	--seed_size 15 \
+	--optimizer \
+	--save_to weight_decay_5.pt"
+
+cat do_test.sh
+$CMD
