9f4dbaf07d9009a4ae4a12d2150f7d8303263889
darkstar112358 <abhishekkadiyan@gmail.com>
2017-05-03 11:24:28 -0400

    Add keras lstm

---
+++ keras_lstm/slurm_keras_tf.sh
@@ -0,0 +1,5 @@
+pip install tensorflow --upgrade --user
+pip install keras --upgrade --user
+pip instal pandas --upgrade --user
+pip install nltk --upgrade --user
+pip install gensim --upgrade --user
