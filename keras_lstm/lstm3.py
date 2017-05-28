a9d372065b1c02b578a071258b8ba44fe4c2b593
Alex Takata <takata.alex@gmail.com>
2017-05-28 13:20:52 -0400

    leak indexing bug

--- keras_lstm/lstm3.py
+++ keras_lstm/lstm3.py
@@ -266,12 +266,12 @@ print('Null word embeddings: %d' % np.sum(np.sum(embedding_matrix, axis=1) == 0)
 
 data_1_train = np.vstack((data_1[idx_train], data_2[idx_train]))
 data_2_train = np.vstack((data_2[idx_train], data_1[idx_train]))
-leaks_train = np.vstack((leaks[idx_train], leaks[idx_train]))
+leaks_train = np.vstack((leaks.iloc[idx_train].values, leaks.iloc[idx_train].values))
 labels_train = np.concatenate((labels[idx_train], labels[idx_train]))
 
 data_1_val = np.vstack((data_1[idx_val], data_2[idx_val]))
 data_2_val = np.vstack((data_2[idx_val], data_1[idx_val]))
-leaks_val = np.vstack((leaks[idx_val], leaks[idx_val]))
+leaks_val = np.vstack((leaks.iloc[idx_val].values, leaks.iloc[idx_val].values))
 labels_val = np.concatenate((labels[idx_val], labels[idx_val]))
 
 weight_val = np.ones(len(labels_val))
